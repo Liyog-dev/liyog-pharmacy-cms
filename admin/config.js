cb36d9f6c79a871b611bbdf7d6be5a6af91c8566
Liyog-dev <liyogbartoos@gmail.com>
2025-07-15 23:54:30 +0100

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,3 +1,10 @@
 // config.js
-const SUPABASE_URL = "";
-const SUPABASE_ANON_KEY = "";
+const SUPABASE_URL = 'https://snwwlewjriuqrodpjhry.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNud3dsZXdqcml1cXJvZHBqaHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDY3MDAsImV4cCI6MjA2ODE4MjcwMH0.WxOmEHxLcEHmMKFjsgrzcb22mPs-sJwW_G3GOuXX2c8';
+
+// Initialize Supabase client
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Buckets
+const IMAGE_BUCKET = 'product-images';
+const VIDEO_BUCKET = 'product-videos';
