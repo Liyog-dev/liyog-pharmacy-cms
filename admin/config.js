fbbdc73e46e27a67d1d1ccba4cd90bbade7de0f7
Liyog-dev <liyogbartoos@gmail.com>
2025-07-16 08:27:22 +0100

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,8 +1,10 @@
 // config.js
 
-export const SUPABASE_URL = 'https://snwwlewjriuqrodpjhry.supabase.co';
+const SUPABASE_URL = 'https://snwwlewjriuqrodpjhry.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNud3dsZXdqcml1cXJvZHBqaHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDY3MDAsImV4cCI6MjA2ODE4MjcwMH0.WxOmEHxLcEHmMKFjsgrzcb22mPs-sJwW_G3GOuXX2c8';
 
-export const SUPABASE_SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNud3dsZXdqcml1cXJvZHBqaHJ5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjYwNjcwMCwiZXhwIjoyMDY4MTgyNzAwfQ.Hdaova_zMvYInRh9CYMMJreMttWskzioe2F4UuH8l3k';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-export const IMAGE_BUCKET = 'product-images';
-export const VIDEO_BUCKET = 'product-videos';
+// Define storage buckets
+const IMAGE_BUCKET = 'product-images';
+const VIDEO_BUCKET = 'product-videos';
